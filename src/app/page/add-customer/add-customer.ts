af73cb7ca7f2676b9d5edc2ab65e1af30588d6a0
sumudumudunayana <mudunayanamihigurathna@gmail.com>
2025-08-22 11:58:59 +0530

    add customer created

---
+++ src/app/page/add-customer/add-customer.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-customer',
+  imports: [],
+  templateUrl: './add-customer.html',
+  styleUrl: './add-customer.css'
+})
+export class AddCustomer {
+
+}
