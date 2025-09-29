1eb94b6a6ae744f24911792a791bda12eeeb93f6
sumudumudunayana <mudunayanamihigurathna@gmail.com>
2025-09-29 22:36:06 +0530

    dash board  created

---
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from "../../common/navbar/navbar.component";
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [NavbarComponent, FormsModule, CommonModule, RouterLink],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css'
+})
+export class DashboardComponent {
+  
+  
+}
+
+
+
